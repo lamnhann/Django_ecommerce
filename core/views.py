2fd8610cca15c97fa1df2b167a28a1f4428c0054
THIENNHAN <93808313+lamnhann@users.noreply.github.com>
2024-01-21 15:34:30 +0700

    - Create html for category/shop page - Create html for the product deetail page

--- core/views.py
+++ core/views.py
@@ -8,3 +8,8 @@ def frontpage(request):
     products = Product.objects.all()[0:8]
     context = {'products': products}
     return render(request, 'core/frontpage.html', context)
+
+def shop(request):
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request, 'core/shop.html', context)
--- ecomerceprj/urls.py
+++ ecomerceprj/urls.py
@@ -17,9 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from core.views import frontpage
+from core.views import frontpage, shop
+from product.views import product
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', frontpage, name='frontpage')
+    path('', frontpage, name='frontpage'),
+    path('shop/', shop, name='shop'),
+    path('product/', product, name='product'),
 ]
