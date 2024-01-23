ffd9c00723e4ca611494bba76525921ce5ab7e91
THIENNHAN <93808313+lamnhann@users.noreply.github.com>
2024-01-23 15:13:17 +0700

    - Make html for the cart page - Make html for the checkout page

--- ecomerceprj/urls.py
+++ ecomerceprj/urls.py
@@ -20,7 +20,7 @@ from django.contrib import admin
 from django.contrib.auth import views
 from django.urls import path, include
 
-from cart.views import add_to_cart
+from cart.views import add_to_cart, cart, checkout
 from core.views import frontpage, shop, signup
 from product.views import product
 
@@ -32,5 +32,7 @@ urlpatterns = [
     path('login/', views.LoginView.as_view(template_name='core/login.html'), name='login'),
     path('shop/', shop, name='shop'),
     path('shop/<slug:slug>/', product, name='product'),
+    path('cart/', cart, name='cart'),
+    path('cart/checkout', checkout, name='checkout'),
     path('add_to_cart/<int:product_id>/', add_to_cart, name='add_to_cart'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
